56708a428d8ca82ad505c1667ca6328d4ef4ca55
DataEngel <miguel.program.73@gmail.com>
2022-06-03 02:18:29 -0500

    model with boto added 1

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,37 @@
-from joblib import load
+import pandas as pd
+import io
+import os
+import boto3
+import pickle  
+import json  
+from joblib import dump, load
 
 import warnings
 warnings.filterwarnings('ignore')
 
-payload = {
-    '5008807': [65, 0, 1, 10000, 1]
-}
 
 def lambda_handler(event, context):
+    
+    d = {
+    '5008807': [65, 0, 1, 10000, 1]
+    }
+    
+    AWS_S3_BUCKET = "engel-tests-20851"
+    #ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    #SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    
+    s3_client = boto3.client(
+        "s3"#,
+        #aws_access_key_id=ACCESS_KEY_ID,
+        #aws_secret_access_key=SECRET_ACCESS_KEY,
+    )
+    filename = "model_risk.joblib"
+    s3_client.download_file(AWS_S3_BUCKET, filename, '/tmp/' + filename)   
+
     my_model = load('/tmp/model_risk.joblib')
-    result = my_model.predict([payload['5008807']]) 
+
+    result = my_model.predict([d['5008807']])
+
     print(result)
 
-
-lambda_handler(payload, context=None)
+lambda_handler(event={}, context=None)
