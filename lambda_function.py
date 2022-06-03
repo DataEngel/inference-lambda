a0ceb7fdb906833e3d7926b30ae007e4174316cd
DataEngel <miguel.program.73@gmail.com>
2022-06-03 01:17:06 -0500

    No such file or directory change 7

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,8 @@ import os
 from os import listdir
 from os.path import isfile, join
 
+from tmp.hi import hello
+
 
 def lambda_handler(event, context):
 
@@ -19,3 +21,6 @@ def lambda_handler(event, context):
     print("Event 2", event)
 
     print("List of path ", onlyfiles)
+
+    hello(33)
+
